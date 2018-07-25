59bfa2416466b87aca2633ab7aa956d66921601b
Jesse <rank_booster@yahoo.com>
2018-07-24 18:12:44 -0600

    Done.

--- stage-4/src/index.js
+++ stage-4/src/index.js
@@ -5,11 +5,14 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import { Provider } from 'react-redux';
 import store from './redux/store';
+import {HashRouter} from 'react-router-dom';
 
 ReactDOM.render(
 
     <Provider store={store}>
+    <HashRouter>
         <App />
+        </HashRouter>
     </Provider>
     , document.getElementById('root'));
 registerServiceWorker();
